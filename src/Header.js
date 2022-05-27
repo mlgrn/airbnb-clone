7461fca1bc6472cb8af8ab6fbab5b3798d58e1aa
E G <emilio@emilioguarino.com>
2022-05-27 01:05:18 -0400

    added components

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="header">
+
+        <img className="header__logo" src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" alt="airbnb logo" />
+        <h1>This is the HEADER component</h1>
+    </div>
+  )
+}
+
+export default Header
