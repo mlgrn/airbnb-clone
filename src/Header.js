650f17582972286074edc76e8e2a265e63f97555
E G <emilio@emilioguarino.com>
2022-05-27 15:57:04 -0400

    got material UI icons working

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
 
 function Header() {
   return (
@@ -7,6 +8,11 @@ function Header() {
 
         <img className="header__logo" src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" alt="airbnb logo" />
         <h1>This is the HEADER component</h1>
+
+        <div className="header__center">
+            <input type="text" />
+            <SearchIcon />
+        </div>
     </div>
   )
 }
