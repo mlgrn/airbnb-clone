32474fb306d957271907850b2471aa3f6cee4067
E G <emilio@emilioguarino.com>
2022-05-27 23:48:41 -0400

    got the calendar picker up and working

--- src/DatePicker.js
+++ src/DatePicker.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './DatePicker.css'
+import { DateRangePicker } from "react-date-range"
+import PeopleIcon from '@material-ui/icons/People';
+import { Button } from "@material-ui/core"
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 
 
-function DatePicker() {
+function DatePicker() { 
+
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection',
+  }
+
+function handleSelect(ranges) {
+  setStartDate(ranges.selection.startDate)
+  setEndDate(ranges.selection.endDate)
+}
+
   return (
-    <div className="datePicker">DATE PICKER</div>
+    <div className="datePicker">
+        <DateRangePicker 
+        ranges={[selectionRange]}
+        onChange={handleSelect}
+        />
+      <h2>Number of guests</h2>
+      <PeopleIcon />
+      <input min={0}
+      defaultValue={2}
+      type="number" />
+    <Button>Search AirBnb</Button>
+
+
+    </div>
   )
 }
 
-export default DatePicker
+export default DatePicker 
