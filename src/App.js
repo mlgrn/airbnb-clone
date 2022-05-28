7343c4a0658abeb74aa4fb8fdfb019a0f15f412d
E G <emilio@emilioguarino.com>
2022-05-28 03:17:11 -0400

    added mediia queries for height and width changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import Header from "./Header"
 import Home from './Home'
 import Footer from './Footer'
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+ 
 function App() {
   return (
     <div className="App">
+  <Router >
+
    <Header />
    <Home />
    <Footer />
+
+  </Router>
+
     </div>
   );
 }
